b4cc10fa789db35e25ba639dbfe08c08260d9717
junyub301 <junyub301@hanmail.net>
2021-06-19 19:08:21 +0900

    node14이후 발생하는  에러 수정

--- shared/shared.utils.js
+++ shared/shared.utils.js
@@ -1,10 +1,9 @@
 import AWS from "aws-sdk";
 
 AWS.config.update({
-    region: "ap-northeast-2",
     credentials: {
         accessKeyId: process.env.AWS_KEY,
-        secretAccessKey: process.env.AWS_SECRET,
+        secretAccessKey: process.env.AWS_SECRET_KEY,
     },
 });
 
@@ -12,7 +11,6 @@ export const uploadPhoto = async (file, userId) => {
     const { filename, createReadStream } = await file;
     const readStream = createReadStream();
     const objectName = `${userId}-${Date.now()}-${filename}`;
-    console.log(userId);
     const upload = await new AWS.S3()
         .upload({
             Bucket: "instaclone-uploads2",
@@ -21,6 +19,5 @@ export const uploadPhoto = async (file, userId) => {
             Body: readStream,
         })
         .promise();
-    console.log(upload);
     return "";
 };
